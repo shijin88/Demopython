a142c7f45d0b31c632c3916b0345db21ef230a21
Shijin Manakkadan <153013131+shijin88@users.noreply.github.com>
2023-12-14 17:51:17 +0530

    Update settings.py

--- todoproject/todoproject/settings.py
+++ todoproject/todoproject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-q&qw@fqq3o@t8$4dfdfc!7phv1g@d(rr)0jkz--7l5w_xu8z2^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
